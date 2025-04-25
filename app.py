369afe12eaaa8771f8a40b53884c309478cbf455
sonofolayiwolaphilip <sonofolayiwolaphili@gmail.com>
2025-04-25 08:47:05 +0100

    removed redundant pages

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/contact', methods=['POST'])
 def contact():
     name = request.form.get('name')
